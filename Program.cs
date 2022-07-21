07bc7f0a93881ee2a005178bb7d8be906cbb6338
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-20 22:56:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace teste_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nomes = new string[] { "Fabricio", "tereza", "macaco" };// Array de nomes (string)
+
+            int[] numeros = new int[] { 1, 2, 3, 4, 5, 6 }; // Array de numeros
+
+            foreach (string teste in nomes)
+            {
+                Console.WriteLine(teste);
+
+            }
+
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+
+            }
+        }
+    }
+}
